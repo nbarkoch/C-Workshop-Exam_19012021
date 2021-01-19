bf720d98f39f2305aad6c8a3db1108d7f0e97ccc
naorb <naorbarkochva@gmail.com>
2021-01-19 13:10:42 +0200

    Add project files.

---
+++ C-Workshop-Exam_19012021/Program.c
@@ -0,0 +1,47 @@
+/* Program.c : This file contains the 'main' function. Program execution begins and ends there. */
+#include <limits.h>
+#include <stdio.h>
+
+/**
+ * @brief the main function
+ * @return integer if ends with success.
+*/
+int main()
+{
+    printf("Hello World!\n");
+}
+
+
+/**
+* @file fileheader.h
+* contains all fuctions for .....
+*/
+
+/*
+* 
+    void Function(void)
+    {
+
+    }
+
+*/
+
+/**
+* @brief: function that does....
+    note: it uses dynamic allocation
+          it uses print to screen
+*/
+//void Function(void);
+
+
+/*
+unsigned choice = 1;
+switch (choice)
+{
+case 1:
+
+case 2:
+
+
+}
+*/
---
+++ README.md
@@ -0,0 +1,8 @@
+# C-Workshop-Exam
+
+Code for a C workshop I've been learning last Semester.
+This exam including files from my VS project.
+
+It follows coding guidelines discussed in the course and touches on subjects I've learned.
+
+Project documentation may be generated using Doxygen - use the Doxyfile in the project folder.
